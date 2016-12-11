cf69fc1d4a0fdb72023205015ef0d621df06e2c9
Yusuf Qedan <yusuf9191@gmail.com>
2016-12-11 13:15:38 -0800

    add user authentication and ability for useres to save their restaurants

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -17,6 +17,7 @@ public class Restaurant {
     private double latitude;
     private double longitude;
     private List<String> categories = new ArrayList<>();
+    private String pushId;
 
     public Restaurant() {}
 
@@ -74,5 +75,13 @@ public class Restaurant {
     public String getLargeImageUrl(String imageUrl) {
         String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 6).concat("o.jpg");
         return largeImageUrl;
+    }
+
+    public String getPushId() {
+        return pushId;
+    }
+
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
     }
 }
